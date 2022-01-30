f90846929b9b044e6e68971c8688b2d2e42374e3
rei <reiballa@gmail.com>
2022-01-30 23:58:56 +0100

    card styling

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -10,15 +10,18 @@ const displayDate = (datestr: string) => {
 }
 
 export default function Article({article} : Props) {
-  // console.log(article)
+  console.log(article)
   return (
   <div className="container p-3 mt-2 bg-zinc-400 text-gray-900 rounded-md shadow-md hover:bg-orange-300 cursor-pointer" 
     onClick={() => window.open(article.url, "_blank")}>
     <div className="title text-lg font-bold">
-        {article.abstract}
+      {article.title}
     </div>
-    <div className="byline text-sm">{article.byline}</div>
-    <div className="date text-sm">{displayDate(article.published_date)}</div>
+    <div className="abstract text-md font-semibold text-zinc-800">
+      {article.abstract}
+    </div>
+    <div className="byline text-sm text-neutral-600">{article.byline}</div>
+    <div className="date text-sm text-blue-900">{displayDate(article.published_date)}</div>
   </div>
   )
 }
